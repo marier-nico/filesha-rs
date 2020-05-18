60c7864de0ea09953e50b17ccb412f82cd7658a9
Nicolas Marier <software@nmarier.com>
2020-05-18 15:29:53 -0400

    Create password hashing helpers

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+#![feature(proc_macro_hygiene, decl_macro)]
+
+#[macro_use] extern crate rocket;
+#[macro_use] extern crate lazy_static;
+
+mod passwords;
+
+
 fn main() {
-    println!("Hello, world!");
+    let hash = passwords::hash_password("thing").unwrap();
+    let hash_2 = passwords::hash_password("thing").unwrap();
+    passwords::verify_password("thing", &hash).unwrap();
+    println!("{}", hash.to_string());
+    println!("{}", hash_2.to_string());
+    rocket::ignite().mount("/", routes![index]).launch();
 }
